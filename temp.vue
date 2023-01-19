cddda0fa63e2dc5da9b138e3ffe8bc8b6fe524ec
DeepKumbhare85 <92006556+DeepKumbhare85@users.noreply.github.com>
2023-01-19 16:30:19 +0530

    Create temp.vue

---
+++ temp.vue
@@ -0,0 +1,79 @@
+<VTimelineItem
+          size="x-small"
+          dot-color="primary"
+        >
+          <!-- 👉 Header -->
+          <div class="d-flex justify-space-between flex-wrap mb-3">
+            <h6 class="text-base font-weight-medium me-3">
+              Create youtube video for next product 😎
+            </h6>
+            <small class="text-xs text-disabled text-no-wrap my-1">Tomorrow</small>
+          </div>
+
+          <p class="mb-0">
+            Product introduction and details video
+          </p>
+
+          <!-- 👉 Avatar & Personal Info -->
+          <span class="d-flex align-bottom mt-2">
+            <VIcon
+              color="error"
+              icon="mdi-play-circle"
+              class="me-2"
+            />
+            <a
+              href="https://www.youtube.com/@themeselection9775"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="text-medium-emphasis font-weight-semibold"
+            >https://www.youtube.com/@themeselection9775</a>
+          </span>
+        </VTimelineItem>
+
+        <VTimelineItem
+          size="x-small"
+          dot-color="success"
+        >
+          <!-- 👉 Header -->
+          <div class="d-flex justify-space-between flex-wrap mb-3">
+            <h6 class="text-base font-weight-medium me-3">
+              Received payment from USA client 😍
+            </h6>
+            <small class="text-xs text-disabled text-no-wrap my-1">January, 10</small>
+          </div>
+
+          <!-- 👉 Content -->
+          <p class="mb-0">
+            Received payment $1,490 for banking ios app
+          </p>
+        </VTimelineItem>
+
+        <VTimelineItem
+          dot-color="error"
+          size="x-small"
+        >
+          <!-- 👉 Header -->
+          <div class="d-flex justify-space-between flex-wrap mb-3">
+            <h6 class="text-base font-weight-medium me-3">
+              Meeting with joseph morgan for next project
+            </h6>
+            <small class="text-xs text-disabled my-1">Wednesday</small>
+          </div>
+
+          <!-- 👉 Content -->
+          <p class="mb-2">
+            Meeting Video call on zoom at 9pm
+          </p>
+          <div class="d-flex align-center">
+            <img
+              :src="pdf"
+              width="24"
+              class="me-2"
+              alt="img"
+            >
+
+            <span class="font-weight-semibold text-sm">
+              presentation.pdf
+            </span>
+          </div>
+        </VTimelineItem>
